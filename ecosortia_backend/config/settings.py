bcbb37f8912ec99f36211603f80a1b2b161ceeed
dhanus22 <smsaidhanush153@gmail.com>
2026-07-28 12:51:19 +0530

    settings

--- ecosortia_backend/config/settings.py
+++ ecosortia_backend/config/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecosortia_db',
-        'USER' : 'postgres',
-        'PASSWORD' : 'postgres',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
